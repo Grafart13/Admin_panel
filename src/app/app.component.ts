094b20c78685b8895520769e82af636af675e111
Grafart13 <dpjarosz@gmail.com>
2019-02-27 13:08:57 +0100

    Adding welcome component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'admin-panel';
+export class AppComponent implements OnInit {
+  whichPanel: number;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.whichPanel = 0; // welcome panel
+  }
+
+  search() {
+    this.whichPanel = 1; // search panel
+  }
+
+  add() {
+    this.whichPanel = 2; // search panel
+  }
+
+  delete() {
+    this.whichPanel = 3; // delete panel
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { SearchComponent } from './components/search/search.component';
+import { AddComponent } from './components/add/add.component';
+import { DeleteComponent } from './components/delete/delete.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    SearchComponent,
+    AddComponent,
+    DeleteComponent
   ],
   imports: [
     BrowserModule,
